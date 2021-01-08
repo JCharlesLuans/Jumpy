fde1a0dd2bc4445b557d4e3fc39c1f836cb3987b
Jean-Charles Luans <luans.jeancharles@gmail.com>
2021-01-08 08:41:13 +0100

    Ajout des icones

--- src/fr/tnlag/jumpy/HUD/Interface.java
+++ src/fr/tnlag/jumpy/HUD/Interface.java
@@ -28,26 +28,22 @@ public class Interface {
      * Nouvelle interface
      */
     public void init(Joueur joueur) throws SlickException {
-        font = new UnicodeFont("/ressource/texture/font/font.ttf", 32, false, false);
-        // font = new UnicodeFont(new java.awt.Font("DejaVu Serif", java.awt.Font.PLAIN, 28));
+        font = new UnicodeFont("/ressource/texture/font/font.ttf", 24, false, false);
         font.addAsciiGlyphs();
         font.addGlyphs(400,600);
-        font.getEffects().add(new ColorEffect(java.awt.Color.WHITE));//Ca sert a quoi ?
+        font.getEffects().add(new ColorEffect(java.awt.Color.WHITE));
         font.loadGlyphs();
 
         this.joueur = joueur;
-        affichagePiece = new Image("ressource/texture/interface/nb_piece.png");
+        affichagePiece = new Image("ressource/texture/interface/icone_piece.png");
     }
 
     public void render(Graphics g) {
 
         g.setFont(font);
-        String text = "" + joueur.getScore();
-
-
-
+        String text = "Pièce : " + joueur.getScore();
         g.resetTransform();
-        font.drawString(126f, 0f, text, Color.white);
+        font.drawString(40, 6f, text, Color.white);
         g.drawImage(this.affichagePiece, 0, 0);
 
     }
